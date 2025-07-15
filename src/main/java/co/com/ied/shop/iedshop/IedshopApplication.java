350a4e5a2aa9454986e984054a5a3b31953b3616
Edgar Hernando Diaz <edgarh2dm3@gmail.com>
2025-07-14 20:40:30 -0500

    first commit

---
+++ src/main/java/co/com/ied/shop/iedshop/IedshopApplication.java
@@ -0,0 +1,13 @@
+package co.com.ied.shop.iedshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IedshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IedshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ied/shop/iedshop/IedshopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ied.shop.iedshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IedshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
